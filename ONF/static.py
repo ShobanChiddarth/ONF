7ec33afcd84e1663da2cf8021add8ad737e02086
Shoban Chiddarth <messageshoban@gmail.com>
2022-01-19 16:36:34 +0530

    Updated static.py
    Closed the file being opened inside `pickle.load()`

--- ONF/static.py
+++ ONF/static.py
@@ -1,6 +1,8 @@
 import pickle, os
 from .main import file
 if os.path.exists(file):
-    count=pickle.load(open(file, 'rb'))
+    fh=open(file, 'rb')
+    count=pickle.load(fh)
+    fh.close()
 else:
     count=0
