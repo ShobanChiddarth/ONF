0fe33bfe5a1f25f66efb0987e344176fb3d7ca32
Shoban Chiddarth <messageshoban@gmail.com>
2022-03-11 21:09:43 +0530

    Updated the algorithm in ONF/main.py
    Updated the algorithm according to ShobanChiddarth/counter@275426d

--- ONF/main.py
+++ ONF/main.py
@@ -11,25 +11,32 @@ count : number of times your computer was turned on
 Windows
 -------
 Put the link of this file in \"onf.cmd\" and put that file in \'shell:startup\''''
+# All the numbers are steps of '### Programming Logic used' in 'README.md'
+# https://github.com/ShobanChiddarth/counter/blob/main/README.md
 import os
 file = os.path.join(os.path.dirname(__file__), 'counterfile.bin') # The file which the 
 # count has to be stored as binary
 if __name__=='__main__':
     import pickle
-    if os.path.exists(file):
+    if os.path.exists(file): # 1
         counterfile=open(file, 'rb')
         count=pickle.load(counterfile) # The count
         # (the number of times your system was turned on)
-    else:
+    else: # 2
         counterfile=open(file, 'wb')
         count=0
         pickle.dump(count, counterfile)
-    counterfile.close()
-    os.remove(file)
-    count+=1
+
+    counterfile.close() # 3
+    count+=1 # 4
+    counterfile=open(counterfile.name, mode='rb+') # 5
+
+    # 6
     counterfile=open(file, 'wb')
-    pickle.dump(count, counterfile)
-    counterfile.close()
+
+    pickle.dump(count, counterfile) # 7
+
+    counterfile.close() # 8
 
 # This file simply counts number of times it was run.
 # Developed from project https://github.com/shobanchiddarth/counter
