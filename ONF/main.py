bc94a59a39283fefe8d2681bfaefa9d14a492518
Shoban Chiddarth <messageshoban@gmail.com>
2022-04-15 18:56:20 +0530

    Removed all comments related to project `counter`
    https://github.com/shobanchiddarth/counter

--- ONF/main.py
+++ ONF/main.py
@@ -2,7 +2,7 @@
 
 DATA
 ----
-file : the link to the file 'counterfile.bin'
+files : the list of links to the files where the data is stored ('counterfile.bin')
 counterfile : file handle of the file 'counterfile.bin'
 count : number of times your computer was turned on
         This is read from binary file 'counterfile.bin' 
@@ -11,32 +11,31 @@ count : number of times your computer was turned on
 Windows
 -------
 Put the link of this file in \"onf.cmd\" and put that file in \'shell:startup\''''
-# All the numbers are steps of '### Programming Logic used' in 'README.md'
-# https://github.com/ShobanChiddarth/counter/blob/main/README.md
+
+
 import os
-file = os.path.join(os.path.dirname(__file__), 'counterfile.bin') # The file which the 
-# count has to be stored as binary
+file = os.path.join(os.path.dirname(__file__), 'counterfile.bin') 
+
 if __name__=='__main__':
     import pickle
-    if os.path.exists(file): # 1
+    if os.path.exists(file): 
         counterfile=open(file, 'rb')
-        count=pickle.load(counterfile) # The count
-        # (the number of times your system was turned on)
-    else: # 2
+        count=pickle.load(counterfile)
+    else:
         counterfile=open(file, 'wb')
         count=0
         pickle.dump(count, counterfile)
 
-    counterfile.close() # 3
-    count+=1 # 4
-    counterfile=open(counterfile.name, mode='rb+') # 5
+    counterfile.close() 
+    count+=1 
+    counterfile=open(counterfile.name, mode='rb+') 
 
-    # 6
+    
     counterfile=open(file, 'wb')
 
-    pickle.dump(count, counterfile) # 7
+    pickle.dump(count, counterfile) 
 
-    counterfile.close() # 8
+    counterfile.close() 
 
 # This file simply counts number of times it was run.
 # Developed from project https://github.com/shobanchiddarth/counter
